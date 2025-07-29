75dd1c31799069410ac60c7e3db7d3a81b560bad
WI$DOM <141811303+CyberwizD@users.noreply.github.com>
2025-07-29 15:49:08 +0100

    feat: update sidebar to implement custom css for glowing effect

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import base64
+import logging
 from utils import qr_scanner
 
 st.set_page_config(
@@ -7,19 +9,63 @@ st.set_page_config(
     layout="wide"
 )
 
-# st.logo(image="static/logo.png", size="small", link="https://github.com/CyberwizD/QR-Auth-System")
+st.logo(image="static/logo.png", size="small", link="https://github.com/CyberwizD/QR-Auth-System")
 
 # Sidebar
-st.sidebar.title("Mobile Authenticator")
+# Insert custom CSS for glowing effect
+st.markdown(
+    """
+    <style>
+    .cover-glow {
+        width: 100%;
+        height: auto;
+        padding: 3px;
+        box-shadow: 
+            0 0 5px #330000,
+            0 0 10px #660000,
+            0 0 15px #990000,
+            0 0 20px #CC0000,
+            0 0 25px #FF0000,
+            0 0 30px #FF3333,
+            0 0 35px #FF6666;
+        position: relative;
+        z-index: -1;
+        border-radius: 45px;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
+
+def img_to_base64(image_path):
+    """Convert image to base64."""
+    try:
+        with open(image_path, "rb") as img_file:
+            return base64.b64encode(img_file.read()).decode()
+    except Exception as e:
+        logging.error(f"Error converting image to base64: {str(e)}")
+        return None
+
+# Load and display sidebar image
+img_path = "static/logo.png"
+img_base64 = img_to_base64(img_path)
+if img_base64:
+    st.sidebar.markdown(
+        f'<img src="data:image/png;base64,{img_base64}" class="cover-glow">',
+        unsafe_allow_html=True,
+    )
 
 # Streamlit UI
-st.title("📷 Mobile QR Code Scanner")
+st.title("Mobile QR Code Scanner")
 st.write("Link your device by scanning the QR code to access your account.")
 
-# Open mobile device camera
-cam_btn = st.button("Open Camera")
+tabs = st.tabs(["📷 Scan QR Code"])
 
-if cam_btn:
-    qr_scanner.qr_scanner()
+with tabs[0]:
+    # Open mobile device camera
+    cam_btn = st.button("Open Camera")
 
-    st.success("QR code scanned successfully!")
+    if cam_btn:
+        qr_scanner.qr_scanner()
+
+        st.success("QR code scanned successfully!")
